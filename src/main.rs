f9e55461cf18975c0518e6aa207cc86ec78681fe
Sönke Liebau <soenke.liebau@gmail.com>
2021-02-05 00:39:27 +0100

    Added passing of pin as command line parameter and signal handling.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use anyhow::anyhow;
 use anyhow::Error;
-use rppal::gpio::{Gpio, Trigger};
+use rppal::gpio::{Gpio, InputPin, Trigger};
 use std::convert::TryFrom;
-use std::fmt;
 use std::fs;
 use std::process::exit;
-use std::time::Duration;
+use std::str::FromStr;
+use std::time::Instant;
+use std::{env, fmt};
+use tokio::signal::unix::{signal, SignalKind};
 
 #[derive(Debug)]
 enum ScreenState {
@@ -13,10 +15,10 @@ enum ScreenState {
     Off,
 }
 
-pub const ON_VALUE: &str = "0\n";
-pub const OFF_VALUE: &str = "1\n";
-pub const STATE_FILE: &str = "/sys/class/backlight/rpi_backlight/bl_power";
-const GPIO_PIN: u8 = 26;
+const ON_VALUE: &str = "0\n";
+const OFF_VALUE: &str = "1\n";
+const STATE_FILE: &str = "/sys/class/backlight/rpi_backlight/bl_power";
+const TRIGGER_DELAY_MS: u128 = 1000;
 
 impl fmt::Display for ScreenState {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -81,8 +83,34 @@ fn check_and_flip() {
     }
 }
 
+fn cleanup(mut pin: InputPin, signal: &str, pin_number: u8) {
+    println!("Got [{}], cleaning up and shutting down.", signal);
+    match pin.clear_async_interrupt() {
+        Ok(()) => println!("Successfully cleared interrupt on pin [{}]", pin_number),
+        Err(e) => println!("Error clearing interrupt on pin [{}]: [{}]", pin_number, e),
+    }
+}
+
 #[tokio::main]
 async fn main() {
+    // Create signal handlers
+    let mut sighup = signal(SignalKind::hangup()).unwrap();
+    let mut sigterm = signal(SignalKind::terminate()).unwrap();
+    let mut sigint = signal(SignalKind::interrupt()).unwrap();
+
+    // Parse pin number from command line
+    let args: Vec<String> = env::args().collect();
+    let pin_number = match args.get(1) {
+        Some(pin_string) => u8::from_str(pin_string).unwrap_or_else(|e| {
+            println!("Unable to parse pin number from string: [{}]", e);
+            exit(-1);
+        }),
+        None => {
+            println!("No port specified!\nUsage: button <port>");
+            exit(-1);
+        }
+    };
+
     // Initialize GPIO communications
     let gpio = Gpio::new().unwrap_or_else(|e| {
         println!("Error initializing gpio communication: [{}]", e);
@@ -91,25 +119,38 @@ async fn main() {
 
     // Create an input pin to use with our button
     let mut pin = gpio
-        .get(GPIO_PIN)
+        .get(pin_number)
         .unwrap_or_else(|e| {
             println!("Error initializing gpio pin: [{}]", e);
             exit(-1);
         })
         .into_input_pullup();
 
-    // Define to reset the pin when the program ends
-    pin.set_reset_on_drop(true);
+    let mut last_triggered = Instant::now();
 
     // Register callback to be executed when the button is pushed
-    match pin.set_async_interrupt(Trigger::FallingEdge, |level| println!("Triggered!")) {
-        Ok(()) => println!("Successfully configured pin {} as button input.", GPIO_PIN),
-        Err(e) => println!("Error occurred when configuring pin {}: [{}]", GPIO_PIN, e),
+    match pin.set_async_interrupt(Trigger::FallingEdge, move |_level| {
+        if last_triggered.elapsed().as_millis() > TRIGGER_DELAY_MS {
+            println!("Recognized button press, flipping screen state.");
+            last_triggered = Instant::now();
+            check_and_flip();
+        }
+    }) {
+        Ok(()) => println!(
+            "Successfully configured pin {} as button input.",
+            pin_number
+        ),
+        Err(e) => println!(
+            "Error occurred when configuring pin {}: [{}]",
+            pin_number, e
+        ),
     }
 
-    // Do nothing, just need to keep the program alive to wait for the callback
-    for _ in 1..4 {
-        tokio::time::sleep(Duration::from_secs(10)).await;
-        println!("Waking for periodic checking of stuff...");
+    // Do nothing, just need to keep the program alive to wait for the callback or until
+    // we are interrupted
+    tokio::select! {
+    _ = sigint.recv() => cleanup(pin, "SIGINT", pin_number),
+    _ = sighup.recv() => cleanup(pin, "SIGHUP", pin_number),
+    _ = sigterm.recv() => cleanup(pin, "SIGTERM", pin_number),
     }
 }
